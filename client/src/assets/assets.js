2391bed2b88840ce2004ee57e742834d9b25b5de
Ayush <itsayushgautam@gmail.com>
2025-11-22 16:27:36 +0530

    Add home page

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,7 +1,11 @@
 import logo from './logo.png';
 import arrow from './arrow.svg';
+import gradientBackground from './gradientBackground.png';
+import star_icon from './star_icon.svg';
 
 export const assets = {
   logo,
-  arrow
+  arrow,
+  gradientBackground,
+  star_icon
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {assets} from '../assets/assets.js'
+
+const Header = () => {
+  return (
+    <div className="mx-8 sm:mx-16 xl:mx-24 relative">
+
+        <div className='text-center my-20 mb-8'>
+
+            <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-blue-300 rounded-full text-sm text-blue-600 hover:bg-blue-100 cursor-pointer'>
+                <p>Ai Feature Integrated</p>
+                <img src={assets.star_icon} className="inline-block w-4" alt="" />
+            </div>
+
+            <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-800'>There is no spoon. <br /> Just <span className='text-blue-600'>code</span>.</h1>
+            <p className='my-6 sm:my-8 max-w-2xl m-auto mx-sm:text-xs text-gray-700'>Peeling back the simulation. Yantra bridges the gap between high-level theory and raw code. Exploring the architecture of AI, one algorithm at a time.</p>
+
+            <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+                <input type="text" placeholder='Search for blogs' className='w-full pl-4 outline-none' required />
+                <button type='submit' className='bg-indigo-600 text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer'>Search</button>
+            </form>
+        </div>
+
+        <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50'/>
+    </div>
+  )
+}
+
+export default Header
