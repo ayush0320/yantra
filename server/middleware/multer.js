71779b14a7518cf02645ebd1160150ae42b7ef57
Ayush <itsayushgautam@gmail.com>
2026-02-11 19:46:50 +0530

    undate multer.js middleware

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,8 +1,9 @@
-import multer from 'multer';
+import multer from "multer";
 
 // Configure multer for handling file uploads
 const upload = multer({
-    storage: multer.diskStorage({})
-})
+  storage: multer.diskStorage({}),
+  limits: { fileSize: 2 * 1024 * 1024 }, // Limit file size to 5MB
+});
 
 export default upload;
